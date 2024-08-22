b265e4b7a73c7bab9ddb2f353767876978497bbc
djschnei21 <32646250+djschnei21@users.noreply.github.com>
2024-08-22 11:40:39 -0400

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   entry: './src/index.js',
